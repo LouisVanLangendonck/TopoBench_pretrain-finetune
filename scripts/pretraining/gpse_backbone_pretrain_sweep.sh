2665a867e4a235b513fc43e1c0fb2b23073cc376
Louis Van Langendonck <louisvanlangendonck@hotmail.com>
2026-06-10 20:38:17 +0000

    Add new ADME datasets

--- scripts/pretraining/gpse_backbone_pretrain_sweep.sh
+++ scripts/pretraining/gpse_backbone_pretrain_sweep.sh
@@ -126,10 +126,12 @@ datasets=(
     # "graph/CYP3A4_Veith"
     # "graph/PROTEINS"
     # "graph/Clearance_Hepatocyte_AZ"
-    # "graph/ogbg-molbace"
-    "graph/ogbg-molhiv"
-    "graph/IMDB-BINARY"
-    "graph/REDDIT-BINARY"
+    # "graph/ogbg-molhiv"
+    # "graph/IMDB-BINARY"
+    # "graph/REDDIT-BINARY"
+    "graph/PPBR_AZ"
+    "graph/CYP2C9_Veith"
+    "graph/Clearance_Microsome_AZ"
     # "graph/Caco2_Wang"
     #graph/NCI1
 )
